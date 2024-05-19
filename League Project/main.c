8128d5daf21b86c7237d3a86c43953a0b9d4f9e8
Gutismall <ari.guter@gmail.com>
2024-05-19 20:14:13 +0300

    Initial Commit

---
+++ League Project/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  League Project
+//
+//  Created by Ari Guterman on 19/05/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
